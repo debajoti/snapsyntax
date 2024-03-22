d928c651493c7ac4934deea176ac1fc4d4775fdd
Debajoti Chakraborty <chakrabortydebajoti@gmail.com>
2024-03-22 17:26:39 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Navbar from './Navbar'
 
 const Hero = () => {
   return (
-    <div>
-      Hero com
-    </div>
+    <>
+      <Navbar />
+      <div className='flex w-full justify-center item-center font-sans font-bold text-[5rem] leading-[4.5rem]'>
+        <div className='text-center font-semibold mt-10'>
+            SENIOR <br /><span className=''>EXPERIENCE</span> <br /> DESIGNER&#169; 
+            <div className='flex flex-row justify-between w-[95%] ml-1'>
+        <p className='text-xs'>lorem ipsum</p>
+        <p className='text-xs'>cool</p>
+            </div>
+        </div>
+      </div>
+        <div className='flex w-full justify-center items-center mt-10'>
+            <img className='rounded-xl' src="/fauzan-saari-CWNXlkekBeA-unsplash.jpg" width={250} height={500} alt="" />
+        </div>
+    </>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <div className='p-5 w-full flex flex-row justify-between font-bold font-sans'>
+    <h2 className='text-3xl'>DevDash</h2>
+        <div className='flex flex-auto m-3 ml-10 gap-5'>
+        <a>About</a>
+        <a>Work</a>
+        </div>
+        <div className='flex justify-right m-3 mr-4 gap-5'>
+        <a>Let's Connect</a>
+        <a>abc@gmail.com</a>
+        </div>
+        </div>
+        </>
   )
 }
 
